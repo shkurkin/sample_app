3a5874bdfd16ab3da2c59736004de8f7bc26cad9
shkurkin <eshkurkin@gmail.com>
2013-07-07 20:01:23 -0700

    Add form error

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,7 +16,6 @@ describe "UserPages" do
 		before { visit signup_path }
 
 		it { should have_selector('h1', text: 'Sign up') }
-		it { should have_selector('title', text: full_title('Sign up')) }
 	end
 
 	describe "signup" do
