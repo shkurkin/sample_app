838c083a74290e04d68e11ff48b128f32f6bff22
shkurkin <eshkurkin@gmail.com>
2013-08-15 17:04:23 -0700

    Update edit system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def find
+    @user = User.new
+  end
+
   def create
   	@user = User.new(params[:user])
   	if @user.save
@@ -16,5 +20,20 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,13 +12,12 @@ RSpec::Matchers.define :have_error_message do |message|
 	end
 end
 
+def sign_in (user)
+	visit signin_path
+	fill_in "Email", with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
 
-#def full_title(page_title)
-#	base_title = "Ruby on Rails Tutorial Sample App"
-#	if page_title.empty?
-#		base_title
-#	else
-#		"#{base_title} | #{page_title}"
-#	end
-#end
+	cookies[:remember_token] = user.remember_token
+end
 
