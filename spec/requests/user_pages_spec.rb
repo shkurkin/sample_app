4513b49c9896af3f713ef45515e722f7c089b6d1
shkurkin <eshkurkin@gmail.com>
2013-08-19 16:52:53 -0700

    Start user index

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,24 @@ describe "UserPages" do
   
 	subject { page }
 
+	describe "index" do
+		before do
+			sign_in FactoryGirl.create(:user)
+			FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+			FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+			visit users_path
+		end
+
+		it { should have_selector('title', text: 'All users') }
+		it { should have_selector('h1',    text: 'All users') }
+
+		it "should list each user" do
+			User.all.each do |user|
+				page.should have_selector('li', text: user.name)
+			end
+		end
+	end
+
 	describe "profile page" do
 	let(:user) { FactoryGirl.create(:user) }
 	before { visit user_path(user) }
@@ -68,7 +86,10 @@ describe "UserPages" do
 
 	describe "edit" do
 		let(:user) { FactoryGirl.create(:user) }
-		before { visit edit_user_path(user) }
+		before do 
+			sign_in user
+			visit edit_user_path(user) 
+		end
 
 		describe "page" do
 			it { should have_selector('h1', text: "Update your profile") }
