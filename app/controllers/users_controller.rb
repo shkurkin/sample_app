5530fad62dacc17489209c2d0a488b400831fa94
shkurkin <eshkurkin@gmail.com>
2013-08-20 11:09:35 -0700

    Finish users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
   def show
   	@user = User.find(params[:id])
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def create
@@ -27,6 +28,12 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "USer destroyed"
+    redirect_to users_url
   end
 
   def edit
@@ -54,5 +61,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
