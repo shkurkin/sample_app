f096c1b527721a06deb04a02258f06050b375852
eli shkurkin <eshkurkin@gmail.com>
2013-08-31 11:03:15 -0700

    Micropost has_many and belongs_to

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,6 +31,7 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -120,7 +121,6 @@ describe User do
   		it { should_not == user_for_invalid_password }
   		specify { user_for_invalid_password.should be_false }
   	end
-
   end
 
   describe "with a password that's too short" do
@@ -141,5 +141,30 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+
+    let! (:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
   end
 end
